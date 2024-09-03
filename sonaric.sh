fde9fb7ea6411c18fdda89c20adb77fadcaa76ed
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-09-03 20:29:57 +0800

    Update sonaric.sh

--- sonaric.sh
+++ sonaric.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# 脚本保存路径
+SCRIPT_PATH="$HOME/sonaric.sh"
+
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
 # 主菜单函数
 function main_menu() {
     while true; do
